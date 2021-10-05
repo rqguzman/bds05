50fc36baef4ee6228dd8cce910728696e03476fb
rqguzman <rafael.guzman@outlook.com>
2021-10-05 10:33:37 -0300

    :white_check_mark: Pass tests

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+-- TEMPLATE USERS
+INSERT INTO tb_user (name, email, password) VALUES ('Bob McGee', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Ana Green', 'ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+-- TEMPLATE ROLES
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+
+-- TEMPLATE USER ROLES
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
